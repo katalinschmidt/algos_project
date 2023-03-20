742949310a2db5ce5fa4a9045eac5ee786f99b86
katalinschmidt <katalin.schmidt.97@gmail.com>
2023-03-20 14:16:06 -0600

    Display array data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// import React from 'react';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 
 function App() {
   return (
-    null
+    <SortingVisualizer></SortingVisualizer>
   );
 }
 
